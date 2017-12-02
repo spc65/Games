6e4e043f115d2b127573973fc0e7e5fcf7bbe211
thomas <tbueti@email.arizona.edu>
2017-12-01 19:53:07 -0700

    Yeah..... controller.php did not have the php brackets....lul

--- pages/controller/controller.php
+++ pages/controller/controller.php
@@ -1,78 +1,79 @@
+<?php
 class DatabaseAdaptor {
-	
+
 	private $DB;
-	
+
 	public function __construct() {
-		
+
 		$database = 'mysql:dbname=hangman; charset=utf8; host=127.0.0.1';
 		$user = 'root';
 		$password = '';
-		
+
 		try {
-			
+
 			$this->DB =  new PDO($database, $user, $password);
 			$this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
-			
+
 			echo ('Error establishing connection');
 			exit();
 		}
 	}
-	
+
 	// Adds a game into the table of games
 	public function createGame($ch_id, $a_id, $word) {
-		
-		$stmt = $this->DB->prepare("INSERT INTO game (challenger_id, 
+
+		$stmt = $this->DB->prepare("INSERT INTO game (challenger_id,
 									acceptor_id, word, num_mistakes, over, won)
 									VALUES (" . $ch_id . ', ' . $a_id . ', ' . $word, . ', 0, FALSE, 0);');
 		$stmt->execute();
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
-	
+
 	// Returns the word progress in the given game.
 	public function getWord($gameId) {
-	
+
 		// Format our query and execute it
 		$stmt = $this->DB->prepare("SELECT word, letters_used FROM game WHERE game_id = " . $gameId . ';');
 		$stmt->execute();
-		
+
 		// Get the word and the letters used so far
 		$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		$word = $results['word'];
 		$letters = $results['letters_used'];
 		$result = '';
-		
+
 		// Let's determine the player's word progress
 		for ($i = 0; $i < strlen($word); $i++) {
-		
+
 			// Did they use this letter?
 			if (strpos($letters, $word[$i])) {
-			
+
 				$result .= $word[$i];
 			}
-			
+
 			// Blank
 			else {
-			
+
 				$result .= '-';
 			}
 		}
-		
+
 		// Return the word progress
 		return $result;
 	}
-	
+
 	// Adds the desired letter to the game's
-	// list of used letters and updates the 
+	// list of used letters and updates the
 	// number of mistakes made in the game,
 	// and determines if the game was won, lost,
 	// or is continuing.
 	public function guess($gameid, $letter) {
-	
+
 		// Add the letter to the list of used letters
 		$stmt = $this->DB->prepare("UPDATE game SET letters_used = letters_used + " . "'" . $letter . "' WHERE game_id = " . $gameid . ";");
 		$stmt->execute();
-		
+
 		// Was this a mistake? Get the word and the letters used to find out.
 		$stmt2 = $this->DB->prepare("SELECT word, letters_used FROM game WHERE game_id =" . $gameid);
 		$stmt2->execute();
@@ -102,16 +103,16 @@ class DatabaseAdaptor {
 
 			// Let's determine the player's word progress
 			for ($i = 0; $i < strlen($word); $i++) {
-		
+
 				// Did they use this letter?
 				if (strpos($letters, $word[$i])) {
-			
+
 					$result .= $word[$i];
 				}
-			
+
 				// Blank
 				else {
-			
+
 					$result .= '-';
 				}
 			}
@@ -132,3 +133,40 @@ class DatabaseAdaptor {
 
 // Here is our database
 $theDBA = new DatabaseAdaptor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
